30d9693481cbe79939d0d4f909c5d20aac6b214a
Luiz França <62324134+Luiz-Soft@users.noreply.github.com>
2024-08-23 04:51:28 +0200

    Fix falty unit test

--- tests/unit/Contact.spec.js
+++ tests/unit/Contact.spec.js
@@ -17,6 +17,7 @@ describe('Contact.vue', () => {
       mocks: {
         $t: (msg) => msg,
       },
+      stubs: ['v-form', 'v-text-field', 'v-btn', 'v-row', 'v-col', 'v-icon'],
     });
   });
 
@@ -27,20 +28,25 @@ describe('Contact.vue', () => {
 
   it('toggles edit mode', async () => {
     expect(wrapper.vm.isEditing).toBe(false);
-    await wrapper.vm.toggleEdit();
+    wrapper.vm.toggleEdit();
+    await wrapper.vm.$nextTick();
     expect(wrapper.vm.isEditing).toBe(true);
   });
 
   it('emits update-contact event when save is clicked', async () => {
+    wrapper.vm.$refs.form = { validate: jest.fn(() => true) };
+    
     wrapper.setData({ isEditing: true });
     await wrapper.vm.saveContact();
+    
     expect(wrapper.vm.isEditing).toBe(false);
+    expect(wrapper.vm.$refs.form.validate).toHaveBeenCalled();
     expect(wrapper.emitted()['update-contact']).toBeTruthy();
+    expect(wrapper.emitted()['update-contact'][0]).toEqual([contact]);
   });
 
   it('emits delete-contact event when delete is clicked', () => {
     wrapper.vm.deleteContact();
     expect(wrapper.emitted()['delete-contact']).toBeTruthy();
   });
-
 });
